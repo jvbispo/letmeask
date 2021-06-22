cafa96151850b66569816a7464f36495759b6ddb
jv <joaovictor.bispo@novaescola.org.br>
2021-06-22 19:06:16 -0300

    crated new room page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Home, NewRoom } from "./pages";
+import "./styles/global.scss";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>test23</h1>
-    </div>
+    <BrowserRouter>
+      <Route component={Home} path='/' exact/>
+      <Route component={NewRoom} path='/rooms/new' />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import ilustrationImg from "../assets/illustration.svg";
+import logoImg from "../assets/logo.svg";
+import logoGoogle from "../assets/google-icon.svg";
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
+import { useHistory } from "react-router-dom";
+
+export const Home = () => {
+  const history = useHistory();
+
+  function navigateToNewRoom() {
+    history.push('/rooms/new');
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={ilustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&A ao vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="letmeask" />
+
+          <Button onClick={navigateToNewRoom} className="create-room">
+            <img src={logoGoogle} alt="google logo" />
+            Crie sua sala com o Google
+          </Button>
+
+          <div className="separator">ou entre em uma sala</div>
+
+          <form action="">
+            <input type="text" placeholder="digite o código da sala" />
+
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+};
